11c532688b40177d6342b94c98d824a5db736ed4
m.muravyeva <marina.muravyeva@sberautopodpiska.ru>
2023-11-27 17:00:39 +0300

    Initial commit

---
+++ src/test/java/api/Specifications.java
@@ -0,0 +1,30 @@
+package api;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+public class Specifications {
+    public static RequestSpecification requestSpecification(String url){
+        return new RequestSpecBuilder()
+                .setBaseUri(url)
+                .setContentType(ContentType.JSON)
+                .setAccept("application/json")
+                .build();
+    }
+
+    public static ResponseSpecification responseSpecification200(){
+        return new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .build();
+    }
+
+    public static void installSpecification(RequestSpecification request,
+                                            ResponseSpecification response){
+        RestAssured.requestSpecification = request;
+        RestAssured.responseSpecification = response;
+    }
+}
---
+++ src/test/java/api/UserTest.java
@@ -0,0 +1,79 @@
+package api;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import java.util.HashMap;
+import java.util.Map;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class UserTest {
+    private static final String URL = "https://petstore.swagger.io/v2/user";
+    private static final String username = "MuravevaTest";
+
+    @Test
+    public void createUser() throws JsonProcessingException {
+        Specifications.installSpecification(Specifications.requestSpecification(URL),
+                Specifications.responseSpecification200());
+
+        UserData userData = new UserData();
+        userData.setId(1);
+        userData.setUsername(username);
+        userData.setFirstName("test");
+        userData.setLastName("test");
+        userData.setEmail("test@test.com");
+        userData.setPassword("qwerty");
+        userData.setPhone("89990000000");
+        userData.setUserStatus(0);
+        String jsonUserData = new ObjectMapper().writeValueAsString(userData);
+        given()
+                .body(jsonUserData)
+                .when()
+                .post("")
+                .then().log().all()
+                .body("message", notNullValue());
+    }
+
+    @Test
+    public void updateUser() throws JsonProcessingException {
+        Specifications.installSpecification(Specifications.requestSpecification(URL),
+                Specifications.responseSpecification200());
+
+        Map<String, String> userUpd = new HashMap<>();
+        userUpd.put("lastName", "testov");
+        userUpd.put("email", "mail@test.ru");
+        userUpd.put("password", "my_password");
+        given()
+                .body(userUpd)
+                .when()
+                .put("/" + username)
+                .then().log().all()
+                .body("message", notNullValue());
+    }
+
+    @Test
+    public void getUserByName() throws JsonProcessingException {
+        Specifications.installSpecification(Specifications.requestSpecification(URL),
+                Specifications.responseSpecification200());
+        given()
+                .when()
+                .get("/" + username)
+                .then().log().all()
+                .body("username", equalTo(username))
+                .extract().response();
+    }
+
+    @Test
+    public void deleteUserByName() throws JsonProcessingException {
+        Specifications.installSpecification(Specifications.requestSpecification(URL),
+                Specifications.responseSpecification200());
+        given()
+                .when()
+                .delete("/" + username)
+                .then().log().all()
+                .body("message", equalTo(username))
+                .extract().response();
+    }
+}
